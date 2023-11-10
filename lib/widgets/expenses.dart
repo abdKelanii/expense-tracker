af06a49a1ee9c5320fc64d310a079aed238cbd72
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2023-11-11 00:57:35 +0300

    101 -> 105

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -25,11 +26,21 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          Text('The chart'),
-          Text('The other thing'),
+          Expanded(
+            child: ExpensesList(expenses: _registredExpenses),
+          ),
         ],
       ),
     );
